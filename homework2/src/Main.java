9ebe48047846f5de6e8906249fb53000a9160567
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-11-18 15:45:17 +0300

    Minor improvements

--- homework2/src/Main.java
+++ homework2/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(" Задача 1");
         FloatInput fInput = new FloatInput();
         float f = fInput.inputFloat();
-        System.out.println("Вы ввели " + f);
+        System.out.println("Вы ввели " + f + "\n");
     }
 }
