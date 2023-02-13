bbe79710481a3188e7f198eaa5e4743de438e2d0
AleksandrKrivoruchko <avk741120@yandex.ru>
2023-02-13 17:12:24 +0300

    Added constructor in PersonsRepository

--- homewok3/src/Main.java
+++ homewok3/src/Main.java
@@ -1,7 +1,13 @@
-
+import model.Person;
+import repository.IRepository;
+import repository.PersonsRepository;
 
 public class Main {
     public static void main(String[] args) {
-
+        PersonsRepository repository = new PersonsRepository();
+        System.out.println(repository.getFilesName().size());
+        for (String str: repository.getFilesName()) {
+            System.out.println(str);
+        }
     }
 }
